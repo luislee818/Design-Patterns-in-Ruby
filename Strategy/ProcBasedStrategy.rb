233d6d5b776b721e16169177ed1162a4a892213e
Dapeng Li <luislee818@gmail.com>
2011-07-05 08:48:11 +0800

    added files for Strategy

---
+++ Strategy/ProcBasedStrategy.rb
@@ -0,0 +1,37 @@
+class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
+
+  def initialize(&formatter)
+    @title = 'Monthly Report'
+    @text = ['Things are going', 'really, really well']
+    @formatter = formatter
+  end
+
+  def output_report
+    @formatter.call(self)
+  end
+end
+
+HTML_FORMATTER = lambda { |context|
+    puts('<html>')
+    puts('  <head>')
+    puts("    <title>#{context.title}</title>")
+    puts('  </head>')
+    puts('  <body>')
+    context.text.each do |line|
+      puts("    <p>#{line}</p>")
+    end
+    puts('  </body>')
+    puts('</html>')
+}
+
+PLAINTEXT_FORMATTER = lambda { |context|
+    puts("**** #{context.title} ****")
+    context.text.each do |line|
+      puts(line)
+    end
+}
+
+report = Report.new &PLAINTEXT_FORMATTER
+report.output_report
